ee8ec790c3c9dc390adff506d50ae9d661d3e084
dynobo <holger.buech@mail.de>
2018-01-12 07:43:51 +0100

    Remove dependencies; Fix actions; Add README

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# snippet_store
-Extension for Albert Launcher, used to store and retrieve coding snippets and other texts
+# Snippet Store
+Extension for [Albert Launcher](https://albertlauncher.github.io/), used to store and retrieve code-snippets or other texts
+
+I coded this, because I like Albert and use it a lot, and I didn't find a nice software for easy access to code snippets stored as plain text files on my drive.
+(IMHO plaintext files has the advantages over DBs or json/xml, that they are easier to sync e.g. with cloud and also accessible/readable without additional applications.)
+
+![screenshot](./snippet_store.png)
+
+## Features
+- Triggered by "sn"
+- Uses a directory with plain-text files as source
+- Search for text in those snippet-files
+- Rank results based on number, position and sourroundings of occurrences
+- Actions:
+   - Copy to clipboard (default)
+   - Open in associated application
+   - Move to Trash bin
+   - Open Snippet-Folder ("sn" without searchterm)
+
+## Dependencies
+- [Send2Trash](https://pypi.python.org/pypi/Send2Trash): `pip install Send2Trash`
+
+## Manual installation
+Download [Snippet Store.py](https://github.com/dynobo/snippet_store/blob/master/Snippet%20Store.py) to one of Albert Launchers folders for python extensions:
+-  ~/.local/share/albert/org.albert.extension.python/modules
+- /usr/local/share/albert/org.albert.extension.python/modules
+- /usr/share/albert/org.albert.extension.python/modules
+
+Adjust the configuration, which is currently hard coded:
+- `SNIPPET_PATH = '/home/holger/cumulus/Notes/snippets'` - Folder, where your snippet files are stored
+- `RECURSIVE = True` - Shall snippet folder be indexed recursively?
+- `SNIPPET_EXT = 'md'` - File extension of your snippet files
+- `iconPath = iconLookup('gedit')` - Icon file to be shown in Albert
+
+Activate in Albert:
+- Open Albert Launcher's Settings
+- Go to "Extensions"
+- Make sure the "Python"-Extension is activated
+- In the Python Extension settings, activate "Snippet Store"
+
+## Snippets
+The first line of the snippet is always used as "Text" for Albert's results list. If the first line starts with '>', this char is stripped.
+
+The second line of the snippet is always used as "Subtext" for Albert's results list. If the first line starts with '>', this char is stripped.
+
+All text below 2nd line goes into the snippet and will be copied to clipboard.
+
+However, **if** the second line starts with a '>', all text below the **3rd** line will be copied to clipboard.
+
+##### Examples:
+```
+> mkconfig update grub
+> Update grub configuration after kernel update to be sure that external keyboard works correctly.
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+```
+
+```
+> Show size of Directory
+sudo du -sh /data/mongodb
+```
+
+For the results, see Screenshot above.
+
+## Todo
+Not sure if I ever do this, as I'm fine with the current state. But maybe if requested or of interest by others:
+- Create new snippet from clipboard
+- Move configuration out of source (not sure to where...)
+- Test performance for many snippets
--- Snippet Store.py
+++ Snippet Store.py
@@ -5,11 +5,9 @@
 Add text snippets as md file in folder, search them, and copy to clipboard."""
 
 from albertv0 import *
-from shutil import which
 import glob
 import re
 import os
-import pyperclip
 import subprocess
 from send2trash import send2trash
 
@@ -19,13 +17,11 @@ __version__ = '0.1'
 __trigger__ = 'sn '
 __author__ = 'dynobo'
 __bin__ = 'sh'
-__dependencies__ = [__bin__, 'send2trash', 'pyperclip']
+__dependencies__ = ['send2trash']
 
-PATH = '/home/holger/cumulus/Notes/snippets'
-
-if which(__bin__) is None:
-    raise Exception('" %s" is not in $PATH.' % __bin__)
-
+SNIPPET_PATH = '/home/holger/cumulus/Notes/snippets'
+RECURSIVE = True
+SNIPPET_EXT = 'md'
 iconPath = iconLookup('gedit')
 
 
@@ -34,7 +30,6 @@ class snippets():
     def __init__(self, *args, **kwargs):
         self.path = args[0]
         self.snippets_store = []
-        self.update_store()
 
     def score(self, query, text):
         score = 0
@@ -45,32 +40,35 @@ class snippets():
 
     def search(self, query):
         results = []
+        # Calculate scores
         for snippet in self.snippets_store:
-            temp_snippet = snippet
-            temp_snippet['score'] = self.score(query, snippet['title']) * 2
-            temp_snippet['score'] += self.score(query, snippet['subtitle']) * 1.5
-            temp_snippet['score'] += self.score(query, snippet['text'])
-            temp_snippet['score'] += self.score(query, snippet['file']) * 0.5
-            if temp_snippet['score'] > 0:
-                results.append(temp_snippet)
+            temp = snippet.copy()
+            temp['score'] = self.score(query, snippet['title']) * 2
+            temp['score'] += self.score(query, snippet['subtitle']) * 1.5
+            temp['score'] += self.score(query, snippet['text'])
+            temp['score'] += self.score(query, snippet['file']) * 0.5
+            if temp['score'] > 0:
+                results.append(temp)
+        # Sort by scores
         results = sorted(results, key=lambda k: k['score'], reverse=True)
-        # print(results)
         return results
 
     def update_store(self):
-        info('{} - Updating Index'.format(__prettyname__))
-        temp_store = []
+        # Clear snippets
+        self.snippets_store = []
+
         # Find markdown files
         files = []
-        for filename in glob.iglob(self.path + '**/*.md', recursive=True):
+        for filename in glob.iglob(self.path + '**/*.{}'.format(SNIPPET_EXT), recursive=RECURSIVE):
             files.append(filename)
 
         # Read markdown files
         for snippet_file in files:
             with open(snippet_file, 'r') as f:
+                # Parse snippet
                 content = f.readlines()
                 filepath = snippet_file.strip()
-                title = content[0][1:].strip()
+                title = content[0].strip('>').strip()
                 if content[1][0] == '>':  # has subtitle
                     subtitle = content[1][1:].strip()
                     text = os.linesep.join(content[2:]).strip()
@@ -78,16 +76,21 @@ class snippets():
                     subtitle = content[1].strip()
                     text = os.linesep.join(content[1:]).strip()
 
-                temp_store.append({
+                # Append to snippet store
+                self.snippets_store.append({
                     'file': filepath,
                     'title': title,
                     'subtitle': subtitle,
                     'text': text,
                 })
-        self.snippets_store = temp_store
+
+        info('[{}] Indexed {} snippet files.'.format(__prettyname__, len(self.snippets_store)))
+
+snippets = snippets(SNIPPET_PATH)
 
 
-snippets = snippets(PATH)
+def initialize():
+    snippets.update_store()
 
 
 def handleQuery(query):
@@ -95,38 +98,41 @@ def handleQuery(query):
     if query.isTriggered:
         try:
             if query.string.strip():
+                def copyToClipboard(text):
+                    p = subprocess.Popen(
+                        ['xclip', '-selection', 'c'], stdin=subprocess.PIPE)
+                    p.communicate(input=bytes(text, 'utf-8'))
+
                 for snippet in snippets.search(query.string.lower()):
                     results.append(
                         Item(id='%s%s' % (__prettyname__, snippet),
                              icon=iconPath,
-                             text=str(snippet['score']) +
-                             ' - ' + snippet['title'],
-                             subtext=snippet['subtitle'][:80],
+                             #text=str(snippet['score']) + ' - ' + snippet['title'],
+                             text=snippet['title'],
+                             subtext=snippet['subtitle'][:120],
                              completion=query.rawString,
                              actions=[
-                                 FuncAction('Copy to Clipboard',
-                                            lambda snippet=snippet: pyperclip.copy(snippet['text'])),
-                                 FuncAction('Open in Editor',
-                                            lambda snippet=snippet: subprocess.Popen(['xdg-open', snippet['file']])),
+                                 ClipAction(text='Copy to Clipboard',
+                                            clipboardText=snippet['text']),
+                                 UrlAction(
+                                     'Open', 'file://{}'.format(snippet['file'])),
                                  FuncAction('Move to Recycle Bin',
                                             lambda snippet=snippet: send2trash(snippet['file']))
                         ]))
             else:
-                def openPath():
-                    subprocess.Popen(['xdg-open', PATH])
-                results.append(
-                    Item(id='%s-create' % __prettyname__,
-                         icon=iconPath,
-                         text=__prettyname__,
-                         subtext='Open Folder containing Snippets',
-                         completion=query.rawString,
-                         actions=[
-                             FuncAction(
-                                 'Create a new snippet', openPath), FuncAction(
-                                 'Update Index', snippets.update_store)
-                         ]
-                         )
-                )
+                results.append(Item(id='%s-create' % __prettyname__,
+                                    icon=iconPath,
+                                    text=__prettyname__,
+                                    subtext='Open Folder containing Snippets',
+                                    completion=query.rawString,
+                                    actions=[
+                                        UrlAction(
+                                            'Open', 'file://{}'.format(SNIPPET_PATH)),
+                                        FuncAction(
+                                            'Update Index', snippets.update_store)
+                                    ]
+                                    )
+                               )
         except Exception as e:
             critical(str(e))
     return results
